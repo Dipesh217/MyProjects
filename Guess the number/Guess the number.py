9ecd7ee7da10936ef6b5ef28cfd1ebe8957900dd
dipesh1 <dipeshjain217@gmail.com>
2021-03-02 20:37:09 +0530

    added a new file

---
+++ Guess the number/Guess the number.py
@@ -0,0 +1,33 @@
+import random
+i = random.randint(1,100)
+chance=1
+print(i)
+
+while True:
+
+    num = int(input("Enter a number:"+"\n"))
+
+    if num == i:
+        print("Congradulation,You have guessed the number.")
+        break
+
+    elif chance==5:
+        print("Game over.You could not guess the number in 5 chances")
+        print("The number is ",i)
+        break
+
+    elif num>100:
+        print("Invalid input:Enter a number from 0 to 100")
+
+    elif num>i:
+        print("Enter a smaller number than ",num)
+        chance = chance + 1
+
+    elif num<i:
+        print("Enter a greater number than ",num)
+        chance = chance + 1
+
+    else:
+        print("Congradulation, you have guessed the number.")
+        break
+
